6d9a684f3ecc22521394ddec8c4044491ce21af4
edutra <edutra@gmail.com>
2021-09-22 03:16:30 -0300

    full Request is done:

--- eventos/eventos/Service/Request.swift
+++ eventos/eventos/Service/Request.swift
@@ -11,19 +11,34 @@ class Request{
     
     let url: URL? = URL(string: "http://5f5a8f24d44d640016169133.mockapi.io/api/events") ?? nil
     
-    func fullEventRequest(completion: @escaping () -> [Event]){
+    func eventsRequest(completion: @escaping ([Event]) -> ()){
         
-        guard let url = self.url else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                print(data)
-            } else if let error = error {
-                print("HTTP Request Failed \(error)")
+        if let url = self.url{
+            let task = URLSession.shared.dataTask(with: url) { (nsData, urlResponse, error) in
+
+                var events: [Event] = []
+                if error == nil{
+                    if let backData = nsData {
+                        do{
+                            let jsonResult = try JSONSerialization.jsonObject(with: backData, options: .allowFragments)
+                        
+                            if let eventJson = jsonResult as? [[String : Any]] {
+                                for event in eventJson{
+                                    
+                                    // Alterar date: Date() por conversão do timestamp
+                                    let eventObject = Event(id: event["id"] as! Int, longitude: event["longitude"] as! Float, latitude: event["latitude"] as! Float, price: event["price"] as! Float, image: event["image"] as! String, title: event["title"] as! String, description: event["description"] as! String, date: Date(), people: [])
+
+                                    events.append(eventObject)
+                                }
+                            }
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                        completion(events)
+                    }
+                }
             }
+            task.resume()
         }
-        
-        task.resume()
-        
     }
 }
